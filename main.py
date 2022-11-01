17b0f88fc822e104ea836c7c8d0bd7ab4613daf2
baidyprod <dima220920041@gmail.com>
2022-11-01 22:31:32 +0200

    V2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import this
-import antigravity
-import __hello__
-from __future__ import braces
+#import this #Opens Python Zen
+import antigravity #Easter egg, opens https://xkcd.com/353/
+import __hello__ #Prints "Hello world!"
+from __future__ import braces #Joke
